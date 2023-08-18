e8ae66e2d4dd854aa681eda232e93abc86da5366
streetcoder <streetkoder@gmail.com>
2023-08-18 16:41:03 -0600

    style applied on all pages

--- seeds/index.js
+++ seeds/index.js
@@ -26,7 +26,7 @@ const seedDB = async () => {
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://unsplash.com/photos/V7uP-XzqX18',
+      image: 'https://images.unsplash.com/photo-1496545672447-f699b503d270?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1471&q=80',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
       price
     });
