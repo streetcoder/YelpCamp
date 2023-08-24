ec7cfc39bbbfe776c5c4badcc6a0a1888158fc67
streetcoder <streetkoder@gmail.com>
2023-08-24 11:26:02 -0600

    delete functionality for reviews and campground

--- app.js
+++ app.js
@@ -119,9 +119,17 @@ app.delete(
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async(req, res) => {
   const campground = await Campground.findById(req.params.id);
   const review = new Review(req.body.review);
+  campground.reviews.push(review);
   await review.save();
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync( async(req, res) => {
+  const { id, reviewId } = req.params;
+  await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+  res.redirect(`/campgrounds/${id}`);
 }))
 
 app.all("*", (req, res, next) => {
