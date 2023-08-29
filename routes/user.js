9f8734d227c369b3945011fbf94678f8009e9ef2
streetcoder <streetkoder@gmail.com>
2023-08-29 06:59:23 -0600

    returnto

--- routes/user.js
+++ routes/user.js
@@ -29,9 +29,11 @@ router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-    req.flash('success', 'Welcome Back!');
-    res.redirect('/campgrounds');
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    res.redirect(req.session.returnTo);
+    // res.redirect(redirectUrl);
 })
 
 router.get('/logout', (req, res, next) => {
