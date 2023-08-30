9983ada735aa18d34d2a9b0d0050eb3aa686c761
streetcoder <streetkoder@gmail.com>
2023-08-30 16:47:55 -0600

    authorized reviews to delete

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const { validateReview } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
@@ -11,10 +11,11 @@ const catchAsync = require("../utils/catchAsync");
 
 router.post(
   "/",
-  validateReview,
+  isLoggedIn, validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -24,7 +25,7 @@ router.post(
 );
 
 router.delete(
-  "/:reviewId",
+  "/:reviewId", isLoggedIn, isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
