d56b97033c4ed134a5879d035c34b39f55d3b75a
streetcoder <streetkoder@gmail.com>
2023-09-01 16:40:49 -0600

    restructuring routes

--- routes/user.js
+++ routes/user.js
@@ -10,9 +10,9 @@ router.get('/register', users.renderRegister);
 
 router.post('/register', catchAsync(users.register))
 
-router.get('/login', users.renderLogin)
-
-router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
+router.route('/login')
+.get(users.renderLogin)
+.post( storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), users.login);
 
 router.get('/logout', users.logout)
 
